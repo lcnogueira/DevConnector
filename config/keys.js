b3a36f496f3a44fb617a0a9a4cf18ec7dba3fd0f
Luiz Cláudio <luizcns18@gmail.com>
2018-07-24 18:19:13 -0300

    feat: Generate token by using jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ru:ampdtbaf0@ds137687.mlab.com:37687/rudbconnector"
+  mongoURI: "mongodb://ru:ampdtbaf0@ds137687.mlab.com:37687/rudbconnector",
+  secretOrKey: "asd08asd98asdas98d09"
 };
