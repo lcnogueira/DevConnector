7ff51d074235c5b63b5b6fdbdf0d0697c57bfeca
Luiz Cláudio <luizcns18@gmail.com>
2018-07-24 10:44:39 -0300

    feat: Add route files

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,8 +20,9 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
