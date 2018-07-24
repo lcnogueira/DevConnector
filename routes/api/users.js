d81c86f08fadbe7754eeb495daaa193f2fca98e4
Luiz Cláudio <luizcns18@gmail.com>
2018-07-24 18:03:02 -0300

    feat: Add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,24 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   POST api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //If the user is not found
+    if (!user) return res.status(404).json({ email: "User not found" });
+
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) res.json({ msg: "Success" });
+      else return res.status(400).json({ password: "Password incorrect" });
+    });
+  });
+});
+
 module.exports = router;
