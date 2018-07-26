582b07b8efdf8d55e3ae313110eb89363c9baeec
Luiz Cláudio <luizcns18@gmail.com>
2018-07-26 18:22:40 -0300

    feat: Create route to delete a comment from a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -149,7 +149,7 @@ router.post(
   }
 );
 
-// @route   POST api/posts/comment/:id
+// @route   POST api/posts/comment/:post_id
 // @desc    Add comment to post
 // @access  Private
 router.post(
@@ -174,6 +174,40 @@ router.post(
         post.comments.unshift(newComment);
 
         // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete(
+  "/comment/:post_id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.post_id)
+      .then(post => {
+        //Check to see if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "Comment does not exist" });
+        }
+
+        //Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //Splice comment out of array
+        post.comments.splice(removeIndex, 1);
+
         post.save().then(post => res.json(post));
       })
       .catch(err => res.status(404).json({ postnotfound: "No post found" }));
