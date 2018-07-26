b2eb841b9f7da60490c854b7598425d4f12a8acd
Luiz Cláudio <luizcns18@gmail.com>
2018-07-26 08:54:00 -0300

    feat: Create route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -22,8 +22,7 @@ router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const errors = {};
-
+    let errors = {};
     Profile.findOne({ user: req.user.id })
       .populate("user", ["name", "avatar"])
       .then(profile => {
@@ -45,7 +44,6 @@ router.get("/handle/:handle", (req, res) => {
   Profile.findOne({ handle: req.params.handle })
     .populate("user", ["name", "avatar"])
     .then(profile => {
-      console.log("this profile->", profile);
       if (!profile) {
         errors.noprofile = "There is no profile for this user";
         res.status(404).json(errors);
@@ -54,6 +52,24 @@ router.get("/handle/:handle", (req, res) => {
       res.json(profile);
     })
     .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get("/all", (req, res) => {
+  let errors = {};
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles" }));
 });
 
 // @route   GET api/profile/user/:user_id
