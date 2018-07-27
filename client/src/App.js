32735346269b4c9d25a17218e5b1c83f382290c5
Luiz Cláudio <luizcns18@gmail.com>
2018-07-27 11:15:13 -0300

    feat: Create links to login and register

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import NavBar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
@@ -12,6 +14,10 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
