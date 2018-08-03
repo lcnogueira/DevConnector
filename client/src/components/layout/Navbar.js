02bcd8bcc89c94d96598c58c5c8cc469dcd765e2
Luiz Cláudio <luizcns18@gmail.com>
2018-08-03 08:55:27 -0300

    feat: Create dashboard and profile redux state

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -19,12 +21,13 @@ class Navbar extends Component {
           <a
             href=""
             onClick={this.onLogoutClick.bind(this)}
-            className="nav-link">
-            <img 
+            className="nav-link"
+          >
+            <img
               className="rounded-circle"
-              src={user.avatar} 
-              alt={user.name} 
-              style={{ width: '25px', marginRight: '5px'}} 
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: "25px", marginRight: "5px" }}
               title="You must have a Gravatar connected to your email to display an image"
             />
             Logout
@@ -89,5 +92,5 @@ const mapStateToProps = ({ auth }) => ({ auth });
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
