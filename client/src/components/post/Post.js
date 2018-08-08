a927e9f7cad06e1560108b714b7d46305d3108fb
Luiz Cláudio <luizcns18@gmail.com>
2018-08-08 13:16:49 -0300

    Add comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../common/Spinner";
+import CommentForm from "./CommentForm";
 import { getPost } from "../../actions/postActions";
 import PostItem from "../posts/PostItem";
 import { Link } from "react-router-dom";
@@ -20,6 +21,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
